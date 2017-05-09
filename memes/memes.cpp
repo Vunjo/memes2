f0464bcdd44c671b5aacb5b16454d0d0cb5c6657
Borna Mihaljevic <borna.mihaljevic@gmail.com>
2017-05-09 21:44:36 +0200

    clean memes.cpp

--- memes/memes.cpp
+++ memes/memes.cpp
@@ -1,11 +1,5 @@
 #include <cstdio> //stdio == standard input output
 
 int main() {
-	int a;
-	int b;
-	float d;
-	scanf("%d%d%f", &a, &b, &d);
-	a = d + a; // a = (int)(d + (float)a);
-	printf("a = %d\nd = %f\n", a, d);
 	return 0;
 }
