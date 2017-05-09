3d9cfc37865050f50e11e9722a4a8526fda84bfd
Borna Mihaljevic <borna.mihaljevic@gmail.com>
2017-05-09 21:42:00 +0200

    remove memes

--- memes/memes.cpp
+++ memes/memes.cpp
@@ -1,13 +1,11 @@
 #include <cstdio> //stdio == standard input output
 
 int main() {
-	int a = 5; //integer - cijeli broj
-	long long d = 10; //integer 64bit
-	float b = 5.5f; //realni broj
-	double c = 5.7; //realni broj 64 bit
-	bool e = true; //true ili false (1 ili 0)
-	char f = '8'; //character
-
-	printf("%d %lld %f %lf %d %c\n", a, d, b, c, e, f);
+	int a;
+	int b;
+	float d;
+	scanf("%d%d%f", &a, &b, &d);
+	a = d + a; // a = (int)(d + (float)a);
+	printf("a = %d\nd = %f\n", a, d);
 	return 0;
 }
