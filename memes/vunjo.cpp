b65e8c6e5f58c05b0351c81bdd2ab0ad0e95b998
Vunjo <VutosORPG@gmail.com>
2017-05-09 22:17:23 +0200

    ja sam bog i isus krist

--- memes/vunjo.cpp
+++ memes/vunjo.cpp
@@ -1,11 +1,19 @@
 #include <cstdio>
+#include <cmath>
 
 int main (){
-    float a;
-    float b;
-    float c;
-    scanf("%f%f", &a,&b);
-    c = a + b;
-    printf("Zbroj = %f\n", c);
+    int a, b, c;
+    float x, d;
+
+    printf("a = ");
+    scanf("%d", &a);
+    printf("b = ");
+    scanf("%d", &b);
+    printf("c = ");
+    scanf("%d", &c);
+    printf("x = ");
+    scanf("%f", &x);
+    d = (a * pow(x,2)) + (b * x) + c;
+    printf("ax^2 + bx + c = %.2f\n", d);
     return 0;
 }
