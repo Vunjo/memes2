c9e9250ddca5b8be6d1f600d6848e5f700668825
Borna Mihaljevic <borna.mihaljevic@gmail.com>
2017-05-09 21:27:14 +0200

    add memes

--- memes/memes.cpp
+++ memes/memes.cpp
@@ -1,9 +1,13 @@
-#include <cstdio>
-#include "bon.h"
-#include "vunjo.h"
+#include <cstdio> //stdio == standard input output
 
-int main(void) {
-	printf("%d\n", memes(5));
+int main() {
+	int a = 5; //integer - cijeli broj
+	long long d = 10; //integer 64bit
+	float b = 5.5f; //realni broj
+	double c = 5.7; //realni broj 64 bit
+	bool e = true; //true ili false (1 ili 0)
+	char f = '8'; //character
+
+	printf("%d %lld %f %lf %d %c\n", a, d, b, c, e, f);
 	return 0;
 }
-
