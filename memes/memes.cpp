62a1a27d27c7613633eb5f57645829713f6e3069
Borna Mihaljevic <borna.mihaljevic@gmail.com>
2017-05-11 23:00:14 +0200

    add for - while

--- memes/memes.cpp
+++ memes/memes.cpp
@@ -1,5 +1,18 @@
 #include <cstdio> //stdio == standard input output
 
 int main() {
+	int x, y;
+	for (int i=0; i<10; i=i+1)
+		printf("%d", i);
+	printf("\n");
+
+	int i = 0;
+	while (i<10) {
+		{
+			printf("%d", i);
+		}
+		i = i+1;
+	}
+	printf("\n");
 	return 0;
 }
